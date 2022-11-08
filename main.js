965a10e1eaa1f0448f67c5cbcf71b7bc043614e1
AdriannaTheCoder123 <100245932+AdriannaTheCoder123@users.noreply.github.com>
2022-11-08 16:28:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(320, 320);
+  canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+   save('myFilterImage.png');
+}
